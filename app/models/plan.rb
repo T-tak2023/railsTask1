5c27a6e52f912dba60a3c081ce5587980c7ac153
USER <user@USERnoMacBook-Pro.local>
2023-12-20 21:50:30 +0900

    開始日終了日のバリデーション不具合を修正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,9 +4,9 @@ class Plan < ApplicationRecord
     validates :end_date, presence:true
     validates :memo, length:{maximum: 500}
     validate :start_end_check
-    def start_end_check
-        if :start_date > :end_date
-            errors.add(:end_date, "は開始日以降の日付を選択してください")  
+        def start_end_check
+            if self.start_date > self.end_date
+                errors.add(:end_date, "は開始日より前の日付で登録できません。")  
+            end
         end
-    end
 end
